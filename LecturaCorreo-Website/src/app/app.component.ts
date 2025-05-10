edd29d792e3f3cd35c72135d96d944deafe30331
Torraskes <78329836+Torraskes@users.noreply.github.com>
2025-05-10 00:00:01 -0600

    feat: Configuracion de componentes: comentario,consulta,ejecutivo

--- LecturaCorreo-Website/src/app/app.component.ts
+++ LecturaCorreo-Website/src/app/app.component.ts
@@ -2,12 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import {ClienteComponent} from './cliente/cliente.component';
+import {ComentarioComponent} from './comentario/comentario.component';
+import {ConsultaComponent} from './consulta/consulta.component';
+import {EjecutivoComponent} from './ejecutivo/ejecutivo.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterOutlet, MainComponent, ClienteComponent],
+  imports: [RouterOutlet,
+    MainComponent,
+    ClienteComponent,
+    ComentarioComponent,
+    ConsultaComponent,
+    EjecutivoComponent],
   standalone: true
 })
 export class AppComponent {
---
+++ LecturaCorreo-Website/src/app/ejecutivo/ejecutivo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejecutivo',
+  standalone: true,
+  templateUrl: './ejecutivo.component.html',
+  styleUrl: './ejecutivo.component.css'
+})
+export class EjecutivoComponent {
+
+}
